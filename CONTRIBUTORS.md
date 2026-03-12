44c239dad838382401f71e71e93399f98b42d1c6
Sam Clarke-Green <74185251+t00sa@users.noreply.github.com>
2026-03-12 22:46:04 +0000

    Port of simit-spack vernier package (#12)

--- CONTRIBUTORS.md
+++ CONTRIBUTORS.md
@@ -4,3 +4,4 @@
 | ----------- | --------------- | ----------- | ---------- |
 | yaswant     | Yaswant Pradhan | Met Office  | 2026-03-03 |
 | ScottWales  | Scott Wales     | Bureau of Meteorology | 2026-03-05 |
+| t00sa       | Sam Clarke-Green | Met Office | 2026-03-11 |
---
+++ spack_repo/metoffice/packages/vernier/package.py
@@ -0,0 +1,77 @@
+# -----------------------------------------------------------------------------
+#  (C) Crown copyright Met Office. All rights reserved.
+#  The file LICENCE, distributed with this code, contains details of the terms
+#  under which the code may be used.
+# -----------------------------------------------------------------------------
+
+from spack_repo.builtin.build_systems.cmake import CMakePackage
+from spack.package import *
+
+
+class Vernier(CMakePackage):
+    """Vernier - profiler for scientific code on HPC platforms."""
+
+    homepage = "https://github.com/MetOffice/Vernier"
+
+    git = "https://github.com/MetOffice/Vernier.git"
+    url = "https://github.com/MetOffice/Vernier/archive/refs/tags/0.3.0.tar.gz"
+    # Head of trunk
+    version("main", branch="main")
+
+    version(
+        "0.4.0",
+        sha256="12bfa7f759517dc566e88287490a423029937f4efdc18bc7a809d8edd37e8464",
+    )
+
+    version(
+        "0.3.1",
+        sha256="76567e028caff5df2e17c0f3cdd2f127794b3d16acf43c64b9d3b762503a6aa2",
+    )
+    version(
+        "0.3.0",
+        sha256="c549fd8ad09d2150e286e1ea25499bda5ecc19467020e505c2ec57c1141afc92",
+    )
+
+    variant("test", default=False, description="enable testing")
+    variant(
+        "max_label_length",
+        default="256",
+        values=("100", "256", "512"),
+        when="@0.4.0:",
+        description="maximum length of caliper labels",
+    )
+
+    depends_on("cmake@3.13:")
+    depends_on("googletest@1.11.0:", when="+test")
+    depends_on("pfunit+mpi", when="+test")
+    depends_on("mpi")
+
+    depends_on("c", type="build")
+    depends_on("cxx", type="build")
+    depends_on("fortran", type="build")
+
+    # See Vernier issue 212
+    conflicts("^openmpi", when="@:0.4")
+
+    def cmake_args(self):
+        args = [
+            self.define("ENABLE_DOXYGEN", False),
+            self.define("ENABLE_SPHINX", False),
+            self.define("INCLUDE_GTEST", False),
+            self.define_from_variant("STRING_LENGTH", "max_label_length"),
+            self.define_from_variant("BUILD_TESTS", "test"),
+            self.define_from_variant("BUILD_FORTRAN_TESTS", "test"),
+        ]
+
+        return args
+
+    def setup_run_environment(self, env):
+        """Setup custom variables in the generated module file"""
+
+        env.prepend_path("FFLAGS", "-I" + self.spec.prefix.include, " ")
+        env.prepend_path("CPPFLAGS", "-I" + self.spec.prefix.include, " ")
+        env.prepend_path(
+            "LDFLAGS",
+            "-L" + self.spec.prefix.lib64 + " -Wl,-rpath=" + self.spec.prefix.lib64,
+            " ",
+        )
