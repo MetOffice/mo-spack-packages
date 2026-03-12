3d4910182d16652f6e3e3071639491b6f8a8920b
Sam Clarke-Green <sam.clarke@metoffice.gov.uk>
2026-03-12 09:01:08 +0000

    Fix problems identified by reviewer
    Add review suggestions.  Also change git attribute to use https rather
    than ssh.

--- spack_repo/metoffice/packages/py_rose_picker/package.py
+++ spack_repo/metoffice/packages/py_rose_picker/package.py
@@ -5,16 +5,20 @@
 
 from spack_repo.builtin.build_systems.python import PythonPackage
 from spack.package import *
-from spack.llnl.util.filesystem import install_tree
 
 
 class PyRosePicker(PythonPackage):
     """py-rose-picker - metadata utility for LFRIC."""
 
     homepage = "https://github.com/MetOffice/rose_picker"
-    url = "https://github.com/MetOffice/rose_picker"
-    git = "git@github.com:MetOffice/rose_picker.git"
+    url = "https://github.com/MetOffice/rose_picker/archive/refs/tags/2026.03.2.tar.gz"
+    git = "https://github.com/MetOffice/rose_picker.git"
 
     version("develop", branch="main")
+    version(
+        "2026.03.2",
+        sha256="9d35cde4edee0069635b8170dd9e017f86f6a8f85b34917fbab545616edd1ce2",
+    )
+
     depends_on("python@3:")
     depends_on("py-setuptools", type="build")
