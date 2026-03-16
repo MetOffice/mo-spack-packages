4b48f8db9b4af749a279f8a8e66a05e29c74474f
Sam Clarke-Green <74185251+t00sa@users.noreply.github.com>
2026-03-16 11:04:29 +0000

    Port of simit-spack sci-fab package (#18)

---
+++ spack_repo/metoffice/packages/py_sci_fab/package.py
@@ -0,0 +1,40 @@
+# -----------------------------------------------------------------------------
+#  (C) Crown copyright Met Office. All rights reserved.
+#  The file LICENCE, distributed with this code, contains details of the terms
+#  under which the code may be used.
+# -----------------------------------------------------------------------------
+
+from spack_repo.builtin.build_systems.python import PythonPackage
+from spack.package import *
+
+
+class PySciFab(PythonPackage):
+    """Fab - A Build System for Tomorrow!
+
+    The Fab build system aims to provide a quick and easy build
+    process tailored towards a specific subset of scientific software
+    developers. Quick should be in both use and operation. Easy should
+    mean the simple things are simple and the complicated things
+    possible.
+    """
+
+    homepage = "https://github.com/MetOffice/fab"
+    pypi = "sci-fab/sci_fab-0.10.1-py3-none-any.whl"
+
+    version(
+        "2.0.1",
+        sha256="70430c2fb88272129866fa9c39e8e38a80a33abc6e8fa78e024c15360b801f54",
+        expand=False,
+    )
+
+    version(
+        "2.0.0",
+        sha256="078566f16bb559e84f5148072931f91c4686049893cb149f3b46f8d66e00e81a",
+        expand=False,
+    )
+
+    variant("clang", default=False, description="enable C support with clang")
+
+    depends_on("python@3.10:")
+    depends_on("py-fparser")
+    depends_on("py-libclang", when="+clang")
