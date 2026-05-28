207d7c16c95c441c4d8c4e2003baec6f5c2ff803
Andrew Coughtrie <24609575+andrewcoughtrie@users.noreply.github.com>
2026-05-28 15:43:16 +0100

    New packages and fixes

---
+++ spack_repo/metoffice/packages/blitz/package.py
@@ -0,0 +1,38 @@
+# -----------------------------------------------------------------------------
+#  (C) Crown copyright Met Office. All rights reserved.
+#  The file LICENCE, distributed with this code, contains details of the terms
+#  under which the code may be used.
+# -----------------------------------------------------------------------------
+from spack.package import depends_on, variant
+
+import spack_repo.builtin.packages.blitz.package as BaseBlitz
+
+
+class Blitz(BaseBlitz.Blitz):
+    """N-dimensional arrays for C++"""
+
+    variant("libpapi", default=False, description="Enable building blitz with PAPI counters")
+
+    depends_on("papi@:5.7", type="link", when="+libpapi")
+
+    def cmake_args(self):
+        args = []
+        if self.spec.satisfies("~libpapi"):
+            args.extend([self.define("BZ_HAVE_LIBPAPI", "")])
+        
+        return args
+
+    def patch(self):
+
+        """Fix compiler vendor detection macros.
+
+        Compiler vendor detection is broken on the Cray EX.  This adds
+        a default to the m4 definition to target llvm when using a
+        recent version of cce.
+        """
+
+        if "%cce" in self.spec and self.compiler.version >= ver(15):
+            # Add a default compiler vendor and set it to llvm if
+            # using a recent Cray compiler
+            filter_file("^\)", "[COMPILER_VENDOR=\"llvm\"]\n)",
+                        "m4/ac_compiler_specific_header.m4")
---
+++ spack_repo/metoffice/packages/py_pydata_sphinx_theme/package.py
@@ -0,0 +1,20 @@
+# -----------------------------------------------------------------------------
+#  (C) Crown copyright Met Office. All rights reserved.
+#  The file LICENCE, distributed with this code, contains details of the terms
+#  under which the code may be used.
+# -----------------------------------------------------------------------------
+
+import spack_repo.builtin.packages.py_pydata_sphinx_theme.package as pyPydataSphinxTheme
+
+from spack.package import version
+
+
+class PyPydataSphinxTheme(pyPydataSphinxTheme.PyPydataSphinxTheme):
+    """A clean, three-column, Bootstrap-based Sphinx theme by and for the PyData community."""
+    
+    version("0.17.1", sha256="2cfc1d926c753c77039b7ee53f0ccebcbee5e81f0db61432b01cbb10ad7fd0af")
+    version("0.17.0", sha256="529c5631582cb3328cf4814fb9eb80611d1704c854406d282a75c9c86e3a1955")
+    
+    
+    
+    
