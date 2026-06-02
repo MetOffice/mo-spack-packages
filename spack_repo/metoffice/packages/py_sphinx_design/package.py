3f9af15c124f26e3e06add7d3ed973b6038cfea3
Andrew Coughtrie <24609575+andrewcoughtrie@users.noreply.github.com>
2026-06-02 14:43:18 +0100

    Ruff fixed

--- spack_repo/metoffice/packages/py_sphinx_design/package.py
+++ spack_repo/metoffice/packages/py_sphinx_design/package.py
@@ -6,7 +6,7 @@
 
 from spack_repo.builtin.build_systems.python import PythonPackage
 
-from spack.package import *
+from spack.package import version, depends_on
 
 
 class PySphinxDesign(PythonPackage):
