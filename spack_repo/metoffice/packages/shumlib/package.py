b8dbf0321a87557de5328672785f9bd8aebb299e
Andrew Coughtrie <24609575+andrewcoughtrie@users.noreply.github.com>
2026-06-30 11:16:50 +0100

    fix lib path (#45)

--- spack_repo/metoffice/packages/shumlib/package.py
+++ spack_repo/metoffice/packages/shumlib/package.py
@@ -69,6 +69,6 @@ class Shumlib(CMakePackage):
         env.prepend_path("CPPFLAGS", "-I" + self.spec.prefix.include, " ")
         env.prepend_path(
             "LDFLAGS",
-            "-L" + self.spec.prefix.lib + " -Wl,-rpath=" + self.spec.prefix.lib,
+            "-L" + self.spec.prefix.lib64 + " -Wl,-rpath=" + self.spec.prefix.lib64,
             " ",
         )
