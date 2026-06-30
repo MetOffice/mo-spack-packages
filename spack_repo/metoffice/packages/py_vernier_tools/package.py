d9d9a18fa93a14c58445b3befcbcf6e2900007cc
Ed Hone <68642690+EdHone@users.noreply.github.com>
2026-06-30 13:32:06 +0100

    New package for Vernier post-processing python library (#41)

---
+++ spack_repo/metoffice/packages/py_vernier_tools/package.py
@@ -0,0 +1,30 @@
+# -----------------------------------------------------------------------------
+#  (C) Crown copyright Met Office. All rights reserved.
+#  The file LICENCE, distributed with this code, contains details of the terms
+#  under which the code may be used.
+# -----------------------------------------------------------------------------
+
+from spack_repo.builtin.build_systems.python import PythonPackage
+from spack.package import depends_on, version
+
+
+class PyVernierTools(PythonPackage):
+    """
+    Python CLI and library for working with Vernier data
+    """
+
+    homepage = "https://github.com/MetOffice/Vernier"
+    git = "https://github.com/MetOffice/Vernier.git"
+    url = "https://github.com/MetOffice/Vernier/archive/refs/tags/0.5.0.tar.gz"
+
+    version("develop", branch="main")
+
+    version(
+        "0.5.0",
+        sha256="be1d2fbb16f30162d07fa80c7a2f03b4e85b07dda3898bff125fda3db8718477",
+    )
+
+    depends_on("python@3.9:")
+    depends_on("py-setuptools", type="build")
+
+    build_directory = "post-processing"
