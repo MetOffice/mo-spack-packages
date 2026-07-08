364e08fd90270b818546a0798510e9636f0e202e
James Bruten <109733895+james-bruten-mo@users.noreply.github.com>
2026-07-08 08:02:03 +0100

    add 3.3.1 line

--- spack_repo/metoffice/packages/py_psyclone/package.py
+++ spack_repo/metoffice/packages/py_psyclone/package.py
@@ -33,6 +33,8 @@ class PyPsyclone(PyPsycloneBase):
     )
 
     depends_on("py-fparser@0.2.4:", type=("build", "run"), when="@3.3.0:")
+    depends_on("py-fparser@0.2.4:", type=("build", "run"), when="@3.3.1:")
 
     # Adjoint fails with sympy >= 1.14
     depends_on("py-sympy@=1.13.3", type=("build", "run"), when="@=3.3.0")
+    depends_on("py-sympy@=1.13.3", type=("build", "run"), when="@=3.3.1")
